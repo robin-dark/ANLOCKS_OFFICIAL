2851dfee1b9f8f70043e33156c360a444719d3d4
robin-dark <80438116+robin-dark@users.noreply.github.com>
2025-12-08 23:04:19 +0530

    Add mobile menu toggle and slider functionality

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Mobile Menu
+function toggleMenu() {
+    document.getElementById("navLinks").classList.toggle("active");
+}
+
+// Slider Auto
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+setInterval(() => {
+    slides[index].classList.remove("active");
+    index = (index + 1) % slides.length;
+    slides[index].classList.add("active");
+}, 3500);
